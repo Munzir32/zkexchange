3429aae117ebe2739707cdb39ae995e7b20e758b
Munzir32 <adeniranm07@gmail.com>
2024-09-01 23:51:24 +0100

    update code

--- packages/nextjs/pages/admin.tsx
+++ packages/nextjs/pages/admin.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import AddTokenModal from '~~/components/AddTokenModal'
 import AccessManager from '~~/components/AccessManager'
+import AccessBuyer from '~~/components/AccessBuyer'
 
 const Admin = () => {
   return (
     <div>
         <AddTokenModal />
         <AccessManager />
+        <AccessBuyer />
     </div>
   )
 }
