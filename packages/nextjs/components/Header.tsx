422619d4923ed4479d35766f8a966451118db6b6
Munzir32 <adeniranm07@gmail.com>
2024-09-01 16:54:57 +0100

    update code

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -20,6 +20,10 @@ export const menuLinks: HeaderMenuLink[] = [
     href: "/",
   },
   {
+    label: "Order",
+    href: "/order",
+  },
+  {
     label: "Debug Contracts",
     href: "/debug",
     icon: <BugAntIcon className="h-4 w-4" />,
---
+++ packages/nextjs/pages/admin.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AddTokenModal from '~~/components/AddTokenModal'
+
+const Admin = () => {
+  return (
+    <div>
+        <AddTokenModal />
+    </div>
+  )
+}
+
+export default Admin
