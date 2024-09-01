e02b5ecca6c5f415905c7a30bba986e19d09ead1
Munzir32 <adeniranm07@gmail.com>
2024-09-02 00:00:13 +0100

    update code

--- packages/nextjs/pages/admin.tsx
+++ packages/nextjs/pages/admin.tsx
@@ -2,13 +2,24 @@ import React from 'react'
 import AddTokenModal from '~~/components/AddTokenModal'
 import AccessManager from '~~/components/AccessManager'
 import AccessBuyer from '~~/components/AccessBuyer'
+import { useAccount } from 'wagmi'
 
 const Admin = () => {
+
+  const { address } = useAccount()
+
   return (
+    
     <div>
-        <AddTokenModal />
-        <AccessManager />
-        <AccessBuyer />
+      {address == "0xbE70b0cE9ecB7c0c4f0be93d8c329212a90BE112" ? 
+      <>
+      <AddTokenModal />
+      <AccessManager />
+      <AccessBuyer />
+      </>
+    : (
+      <p>Connect Admin wallet to contiue</p>
+    )}
     </div>
   )
 }
